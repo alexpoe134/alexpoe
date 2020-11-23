7c9d73f10f4ec3392c2c501409e0744d3ffdb7f8
alexpoe134 <alexanderpoe31@gmail.com>
2020-11-23 07:14:35 -0600

    revamped website for development instead of music
    adding my projects here from now on

---
+++ javascript/buttonclick.js
@@ -0,0 +1,14 @@
+function buttonClick(){
+  $.ajax({
+    url: 'http://www.omdbapi.com/?s=' + encodeURI(document.getElementById("search").value) + '&apikey=c0fc1fcc',
+    dataType: 'json',
+    type: 'get',
+    cache: false,
+    success: function(data) {
+      $(data).each(function(index, value){
+        document.getElementById("poster").src = (value.Search[0].Poster);
+        //console.log(value.Search[0].Poster);
+      });
+    }
+  });
+}
